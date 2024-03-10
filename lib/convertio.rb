805d24ab0b5dbfabb3766b319402df9c98549510
Aparna Gopal <aparnagopalmr@gmail.com>
2024-03-10 14:03:34 +0530

    initial converter methods

--- lib/convertio.rb
+++ lib/convertio.rb
@@ -2,7 +2,22 @@
 
 require_relative "convertio/version"
 
+# Convert value to different units
 module Convertio
   class Error < StandardError; end
-  # Your code goes here...
+  ABBREVIATIONS = {
+    'km': "kilometers",
+    'mi': "miles"
+  }.freeze
+  KILOMETERS_PER_MILE = 1.60934
+  def self.convert(value, from:, to:)
+    case [from, to]
+    when %i[mi km]
+      value * KILOMETERS_PER_MILE
+    when %i[km mi]
+      value / KILOMETERS_PER_MILE
+    else
+      raise Error, "Unsupported conversion: #{ABBREVIATIONS[from]} to #{ABBREVIATIONS[to]}"
+    end
+  end
 end
--- spec/convertio_spec.rb
+++ spec/convertio_spec.rb
@@ -5,7 +5,17 @@ RSpec.describe Convertio do
     expect(Convertio::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "#convert" do
+    it "support miles to kilometers" do
+      expect(described_class.convert(1, from: :mi, to: :km)).to be_within(0.001).of(1.60934)
+    end
+
+    it "supports kilometers to miles" do
+      expect(described_class.convert(1, from: :km, to: :mi)).to be_within(0.001).of(0.621371)
+    end
+
+    it "throws an error for invalid conversions" do
+      expect { described_class.convert(1, from: :l, to: :km) }.to raise_error(Convertio::Error)
+    end
   end
 end
